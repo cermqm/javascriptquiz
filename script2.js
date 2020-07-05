1b98aecaa7ab807ba3e46e5579647f70807beca6
Michael Mink <mmink@michaelamink.com>
2020-07-05 01:32:30 -0500

    adding displayhighscores and script3 - to display high scores

--- script2.js
+++ script2.js
@@ -12,11 +12,7 @@ var ini = "";
 
 writequestions();
 
-
-// changing to jquery methods...
-
 function writeqandmc(z) {
-    // console.log("questions[z].qu = " + questions[z].qu);
 
     $('#q').html(questions[z].qu);
     $('#c1').html(questions[z].mc1);
@@ -37,15 +33,11 @@ function submit() {
     for (var x = 0, length = radios.length; x < length; x++) {
         if (radios[x].checked) {
             val = radios[x].value;
-            // console.log("val = " + val);
             break;
         }
     }
-    // console.log("z = " + z);
-    // console.log("val = " + val);
-    // console.log("questions[z] = " + questions[z].a);
+
     if (questions[z].a === val) {
-        // console.log("questions[z].a = " + questions[z].a);
         $("#rorw").html("Your Answer is Correct...")
         score++
         $("#s").html("Score = " + score);
@@ -79,24 +71,6 @@ function submit() {
 
         // ------------------------------------------------------------------------------------
 
-        // var saveb = document.createElement("button");
-        // saveb.setAttribute("id", "savehs");
-        // saveb.innerHTML = "Save";
-        // document.body.appendChild(saveb);
-
-        // $("body").append("</br>");
-
-
-        // document.getElementById("savehs").addEventListener("click", function(event) {
-        //     // var initials = document.getElementById("ini").innerHTML;
-        //     var initials = document.querySelector("#ini");
-        //     console.log("initials in save button = " + initials.value);
-        //     writescore(initials.value);
-        // });
-
-        // ------------------------------------------------------------------------------------
-
-
     }
     val = "";
 
@@ -104,7 +78,6 @@ function submit() {
 
 function writescore(ini) {
     storeHighScore(score, ini);
-    // renderHighScore();
 }
 
 function cleanuphtml() {
@@ -116,8 +89,6 @@ function cleanuphtml() {
 }
 
 function startq() {
-    // event.preventDefault();
-    // cleanuphtml(); //clear the page
     firstTime = 1;
     qph(); //write question place holders
     starttimer();
@@ -146,46 +117,3 @@ function storeHighScore(score, ini) {
     localStorage.setItem("highScore", JSON.stringify(highScore));
     console.log("highScore = " + highScore);
 }
-
-function renderHighScore() {
-
-    // var lihighScore = document.createElement("h2");
-
-    // pushhighScoreLabel = document.createElement("h2");
-    // pushhighScoreLabel.innerHTML = "High Scores";
-    // document.body.append(pushhighScoreLabel);
-
-    // var dhighScore = document.querySelector("#dhS")
-    // dhighScore.innerHTML = "";
-    var container = document.createElement("div");
-
-    for (let i = 0; i < highScore.length; i++) {
-        var displayhighScore = highScore[i];
-        var lihighScore = document.createElement("h2");
-
-        lihighScore.textContent = displayhighScore;
-        lihighScore.setAttribute("data-index", i);
-
-        var button = document.createElement("button");
-        button.textContent = "Delete";
-
-        lihighScore.appendChild(button);
-        container.appendChild(lihighScore);
-    }
-
-    // dhighScore.appendChild(container);
-}
-
-
-// button.getElementById("startquiz").addEventListener("click", function(event) {
-//     event.preventDefault();
-//     var element = event.target;
-
-//     if (element.matches("button") === true) {
-//         var index = element.parentElement.getAttribute("data-index");
-//         highScores.splice(index, 1);
-//         storeHighScore();
-//         renderHighScore();
-//     }
-
-// });
---
+++ script3.js
@@ -0,0 +1,56 @@
+// Declare Global Variables
+var highScoreList = document.querySelector("#hs");
+var highScore = [];
+var dhighScore = document.querySelector("#dhS")
+
+
+var z = 0;
+firstTime = 0;
+score = 0;
+timeleft = 450;
+var ini = "";
+
+renderHighScore();
+
+function renderHighScore() {
+
+    // var lihighScore = document.createElement("h2");
+
+    // pushhighScoreLabel = document.createElement("h2");
+    // pushhighScoreLabel.innerHTML = "High Scores";
+    // document.body.append(pushhighScoreLabel);
+
+    // var dhighScore = document.querySelector("#dhS")
+    // dhighScore.innerHTML = "";
+    var container = document.createElement("div");
+
+    for (let i = 0; i < highScore.length; i++) {
+        var displayhighScore = highScore[i];
+        var lihighScore = document.createElement("h2");
+
+        lihighScore.textContent = displayhighScore;
+        lihighScore.setAttribute("data-index", i);
+
+        var button = document.createElement("button");
+        button.textContent = "Delete";
+
+        lihighScore.appendChild(button);
+        container.appendChild(lihighScore);
+    }
+
+    dhighScore.appendChild(container);
+}
+
+
+// button.getElementById("startquiz").addEventListener("click", function(event) {
+//     event.preventDefault();
+//     var element = event.target;
+
+//     if (element.matches("button") === true) {
+//         var index = element.parentElement.getAttribute("data-index");
+//         highScores.splice(index, 1);
+//         storeHighScore();
+//         renderHighScore();
+//     }
+
+// });
