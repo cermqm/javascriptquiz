f05391f01a5ff55f56d467399da5edcc3322d9f2
Michael Mink <mmink@michaelamink.com>
2020-07-05 09:43:19 -0500

    working through display of high score - added the init function to read local storage

--- script3.js
+++ script3.js
@@ -10,36 +10,43 @@ score = 0;
 timeleft = 450;
 var ini = "";
 
+init();
+console.log("highScore = " + highScore);
 renderHighScore();
+
+
+function init(parameter) {
+    var storedHighScore = JSON.parse(localStorage.getItem("highScore"));
+    console.log("storedHighScore = " + storedHighScore);
+    if (storedHighScore !== null) {
+        highScore = storedHighScore;
+    }
+}
 
 function renderHighScore() {
 
-    // var lihighScore = document.createElement("h2");
 
     $("<div>").insertAfter("#insidecard").attr({ id: "ic1" }).html("</br>");
     $("<h2>").insertAfter("#ic1").attr({ id: "ic2" }).html("Top 5 High Scores");
     $("<h4>").insertAfter("#ic2").attr({ id: "dhS" });
 
-    // pushhighScoreLabel = document.createElement("h2");
-    // pushhighScoreLabel.innerHTML = "High Scores";
-    // document.body.append(pushhighScoreLabel);
-
     var dhighScore = document.querySelector("#dhS")
-    dhighScore.innerHTML = "";
+    $("#dhS").empty();
     var container = document.createElement("div");
 
     for (let i = 0; i < highScore.length; i++) {
         var displayhighScore = highScore[i];
-        var lihighScore = document.createElement("h2");
+        console.log("highScore[i] = " + highScore[i])
+            // var lihighScore = document.createElement("h2");
+            // lihighScore.textContent = displayhighScore;
+            // lihighScore.setAttribute("data-index", i);
 
-        lihighScore.textContent = displayhighScore;
-        lihighScore.setAttribute("data-index", i);
+        // var button = document.createElement("button");
+        // button.textContent = "Delete";
 
-        var button = document.createElement("button");
-        button.textContent = "Delete";
-
-        lihighScore.appendChild(button);
-        container.appendChild(lihighScore);
+        // lihighScore.appendChild(button);
+        // container.appendChild(lihighScore);
+        $("<h2>").insertAfter("#dhS").attr({ id: "dhS2" }).html("Highscore - " + highScore[i]);
     }
 
     dhighScore.appendChild(container);
